68169d4c3117bd82df0495ff335bcd1b919ada93
leenak0 <cdp90@naver.com>
2019-05-22 04:35:19 +0900

    안드로이드 ver1

---
+++ autoblur/app/src/main/java/com/leenak0/project/autoblur/MainActivity.java
@@ -0,0 +1,39 @@
+package com.leenak0.project.autoblur;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton button1 = (ImageButton)findViewById(R.id.Main_button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent face_register = new Intent(getApplicationContext(),FaceRegister.class);
+                startActivity(face_register);
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
